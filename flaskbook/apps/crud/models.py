049162b196a6493574649137f31a0f30edfff2a4
shimooo3 <125008729+shimooo3@users.noreply.github.com>
2023-06-14 15:00:15 +0900

    5章終了　RDBの操作説明

--- flaskbook/apps/crud/models.py
+++ flaskbook/apps/crud/models.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String)
     created_at = db.Column(db.DateTime, default = datetime.now)
     updated_at = db.Column(db.DateTime, default = datetime.now, onupdate = datetime.now)
+    user_images = db.relationship("UserImage", backref = "user", order_by="desc(UserImage.id)")
     
     @property
     def password(self):
