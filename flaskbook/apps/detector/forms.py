a622ad5065bbcca6b12359a313d086c9be0f3315
shimooo3 <125008729+shimooo3@users.noreply.github.com>
2023-06-19 18:07:22 +0900

    7章終了　画像のアップロード

---
+++ flaskbook/apps/detector/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from flask_wtf.form import FlaskForm
+from wtforms.fields.simple import TextAreaField
+from wtforms import SubmitField
+
+class UploadImageForm(FlaskForm):
+    image = FileField(
+        validators=[
+            FileRequired("choose a picture"),
+            FileAllowed(["png", "jpg", "jpeg"], "not allowed")
+        ]
+    )
+    submit = SubmitField("Upload")
--- flaskbook/apps/detector/views.py
+++ flaskbook/apps/detector/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app, send_from_directory, url_for, redirect
 from apps.app import db
 from apps.crud.models import User
 from apps.detector.models import UserImage
+from fileinput import filename
+import uuid
+from pathlib import Path
+from flask_login import current_user, login_required
+from apps.detector.forms import UploadImageForm
 
 dt = Blueprint("detector", __name__, template_folder="templates")
 
@@ -11,3 +16,25 @@ def index():
         db.session.query(User, UserImage).join(UserImage).filter(User.id == UserImage.user_id).all()
     )
     return render_template("detector/index.html", user_images=user_images)
+
+@dt.route("/images/<path:filename>")
+def image_file(filename):
+    return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename)
+
+@dt.route("/upload", methods=["GET", "POST"])
+def upload_image():
+    form = UploadImageForm()
+    if form.validate_on_submit():
+        file = form.image.data
+        ext = Path(file.filename).suffix
+        image_uuid_file_name = str(uuid.uuid4()) + ext
+        
+        image_path = Path(current_app.config["UPLOAD_FOLDER"], image_uuid_file_name)
+        file.save(image_path)
+        
+        user_image = UserImage(user_id = current_user.id, image_path = image_uuid_file_name)
+        db.session.add(user_image)
+        db.session.commit()
+        
+        return redirect(url_for("detector.index"))
+    return render_template("detector/upload.html", form = form)
